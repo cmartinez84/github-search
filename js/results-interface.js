27860b6acb0fc480db457213f1eb21a8d097a6bd
Chris Martinez <cardamomclouds@yahoo.com>
2016-10-14 13:28:30 -0700

    add repos to results on page

--- js/results-interface.js
+++ js/results-interface.js
@@ -4,14 +4,20 @@ function GithubSearch(){
 
 }
 GithubSearch.prototype.search = function(searchName){
-    $.get('https://api.github.com/users/'+searchName+'?access_token=' + apiKey).then(function(response){
+    $.get('https://api.github.com/users/' +searchName +'?access_token=' + apiKey).then(function(response){
         $("#userName").text(response.login);
         $("#fullName").text(response.name);
         $("#email").text(response.email);
         $("#link").attr("href", response.html_url);
         $("#avatar").attr("src", response.avatar_url);
-        var repos = $.getJSON(response.repos_url);
-
+        $.getJSON(response.repos_url, function(data){
+            // var items = [];
+            $.each(data, function(key, value){
+                console.log(key, value);
+                $("#repos").append("<li>"+value.name+"</li>")
+            });
+        });
+        // respos.responseJSON;
         console.log(repos);
     }).fail(function(error){
         console.log(error.responseJSON.message);

88722973891f43868d6bf4053be5ce89b4370732
Chris Martinez <cardamomclouds@yahoo.com>
2016-10-14 12:59:07 -0700

    display name, username, ink and avatar as results

--- js/results-interface.js
+++ js/results-interface.js
@@ -1,11 +1,18 @@
 var apiKey = require('./../.env').apiKey;
 
 function GithubSearch(){
+
 }
 GithubSearch.prototype.search = function(searchName){
     $.get('https://api.github.com/users/'+searchName+'?access_token=' + apiKey).then(function(response){
-        var stuff = JSON.stringify(response);
-        $(".well").append("<div>"+stuff+"</div>");
+        $("#userName").text(response.login);
+        $("#fullName").text(response.name);
+        $("#email").text(response.email);
+        $("#link").attr("href", response.html_url);
+        $("#avatar").attr("src", response.avatar_url);
+        var repos = $.getJSON(response.repos_url);
+
+        console.log(repos);
     }).fail(function(error){
         console.log(error.responseJSON.message);
         alert("sorry, no username was found with the name" + searchName+" , please try again.");
--- js/submit-interface.js
+++ js/submit-interface.js
@@ -1,11 +1,13 @@
-var GithubSearch = require('./../js/search.js').githubSearchModule;
+var GithubSearch = require('./../js/results-interface.js').githubSearchModule;
 
 $(function(){
     var githubSearch = new GithubSearch();
 
     $("button").click(function(){
         var searchName = $("#searchName").val();
-        githubSearch.search(searchName);
-        // console.log(searchName);
+        response= githubSearch.search(searchName);
+        console.log(response);
+        // console.log(JSON.stringify(response));
+        // console.log(githubSearch.muffins);
     });
 });
